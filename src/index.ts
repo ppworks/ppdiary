47792c8d566f321a706e584709828d7c0a4314ec
ppworks <naoto5959@gmail.com>
2025-08-28 20:58:45 +0900

    Setup MCP server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+async function main() {
+
+  const server = new McpServer({
+    name: "ppdiary",
+    version: "1.0.0",
+  });
+
+  const transport = new StdioServerTransport();
+
+  transport.onclose = () => {
+    console.error("Transport closed");
+  };
+
+  transport.onerror = (error) => {
+    console.error("Transport error:", error);
+  };
+
+  await server.connect(transport);
+}
+
+await main().catch((_error) => {
+  process.exit(1);
+});
