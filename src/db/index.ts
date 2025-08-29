55f39dc7abeb7e4c19b4b582a45c611490450ee9
ppworks <naoto5959@gmail.com>
2025-08-30 02:37:04 +0900

    Fix migration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
 import { execSync } from "node:child_process";
-import { existsSync } from "node:fs";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
@@ -12,10 +11,8 @@ export const db = drizzle(sqlite, { schema });
 if (dbPath === ":memory:") {
   migrate(db, { migrationsFolder: "./src/db/migrations" });
 } else {
-  if (!existsSync(dbPath)) {
-    execSync("npx drizzle-kit migrate", {
-      stdio: ["ignore", "ignore", "pipe"], // Suppress progress output
-      env: { ...process.env, NO_COLOR: "1", DATABASE_URL: dbPath },
-    });
-  }
+  execSync("npx drizzle-kit migrate", {
+    stdio: ["ignore", "ignore", "pipe"], // Suppress progress output
+    env: { ...process.env, NO_COLOR: "1", DATABASE_URL: dbPath },
+  });
 }
